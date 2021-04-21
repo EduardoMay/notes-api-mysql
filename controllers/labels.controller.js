7f7909207a341065021cd6934d9ca2997ac943e5
EduardoMay <eduardo_may@outlook.com>
2021-04-20 22:15:44 -0500

    feat: metodo post en Notes

--- controllers/labels.controller.js
+++ controllers/labels.controller.js
@@ -6,10 +6,26 @@ const getAll = async (req, res) => {
 
     res.status(200).json(data);
   } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
+const post = async ({ body }, res) => {
+  try {
+    const { data } = body;
+
+    const result = await Labels.query().insertGraph(data);
+
+    console.log(result);
+    res.status(200).json({ result });
+  } catch (error) {
+    console.log(error);
     res.status(500).send(error);
   }
 };
 
 module.exports = {
-  getAll
+  getAll,
+  post
 };
--- routes/labels.js
+++ routes/labels.js
@@ -4,5 +4,6 @@ const labelsController = require("../controllers/labels.controller");
 const router = new Router();
 
 router.get("/", labelsController.getAll);
+router.post("/", labelsController.post);
 
 module.exports = router;
