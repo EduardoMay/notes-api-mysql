14c967104e9e8ff2bc032fa49020c4362569f0c4
EduardoMay <eduardo_may@outlook.com>
2021-04-23 10:41:33 -0500

    feat: Crear tabla notes_labels
    Crear tabla notes_labels para poder tener varias etiquetan en una nota

--- database/migrations/20210423032837_create_notes_labels_table.js
+++ database/migrations/20210423032837_create_notes_labels_table.js
@@ -1,4 +1,11 @@
-// TODO crear tabla notes_label
-exports.up = function (knex) {};
+exports.up = async function (knex) {
+  return await knex.schema.createTable("notes_labels", function (table) {
+    table.increments("id").primary();
+    table.integer("id_note").unsigned().references("id").inTable("notes");
+    table.integer("id_label").unsigned().references("id").inTable("labels");
+    table.datetime("created_at").defaultTo(knex.fn.now());
+    table.datetime("updated_at").defaultTo(knex.fn.now());
+  });
+};
 
 exports.down = function (knex) {};
